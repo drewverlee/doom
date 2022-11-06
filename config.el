448d3952734234793cffc808262db86ce114f61f
drew <drew.verlee@gmail.com>
2022-11-06 05:42:51 -0600

    fuzzy search

--- config.el
+++ config.el
@@ -117,3 +117,5 @@
 
 (setq tab-always-indent 'complete)
 
+(add-hook 'cider-repl-mode-hook #'cider-company-enable-fuzzy-completion)
+(add-hook 'cider-mode-hook #'cider-company-enable-fuzzy-completion)
